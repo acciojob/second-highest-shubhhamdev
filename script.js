745f822f05f7e3478ae6629dc1e7c33ddac456ac
Shubham Dev <133051223+shubhhamdev@users.noreply.github.com>
2023-07-16 22:40:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+  if (arr.length < 2 || new Set(arr).size === 1) {
+    return -Infinity;
+  }
+  
+  let max = -Infinity;
+  let secondMax = -Infinity;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      secondMax = max;
+      max = arr[i];
+    } else if (arr[i] > secondMax && arr[i] < max) {
+      secondMax = arr[i];
+    }
+  }
+  
+  return secondMax;
+}
